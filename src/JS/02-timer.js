89582ef7431ff62cc1cb7d78bfc489bf3f32746d
igor-rizak <igor.rizak@ukr.net>
2023-05-11 11:14:23 +0300

    commit

--- src/JS/02-timer.js
+++ src/JS/02-timer.js
@@ -6,19 +6,42 @@ import Notiflix from 'notiflix';
 const refs = {
     dateTimeInput: document.querySelector("#datetime-picker"),
     btnStart: document.querySelector("[data-start]"),
+    btnReset: document.querySelector("[data-reset]"),
     days: document.querySelector("span[data-days]"),
     hours: document.querySelector("span[data-hours]"),
     minutes: document.querySelector("span[data-minutes]"),
     seconds: document.querySelector("span[data-seconds]"),
 }
 
-// console.log(refs.dateTimeInput)
+refs.btnReset.addEventListener('click', resetTimer);
+
+function resetTimer() {
+    refs.btnReset.disabled = true;
+    refs.dateTimeInput.value = '';
+    clearInterval(timerId);
+    refs.days.textContent = '00';
+    refs.hours.textContent = '00';
+    refs.minutes.textContent = '00';
+    refs.seconds.textContent = '00';
+    refs.dateTimeInput.disabled = false; 
+}
+
 
 const CURRENT_DATE = new Date();
 let SELECTED_DATE = new Date();
 let delta;
+let timerId;
 
 refs.btnStart.disabled = true;
+refs.btnReset.disabled = true;
+
+
+function startTimer() {
+    refs.btnStart.disabled = true;
+    refs.btnReset.disabled = false;
+    refs.dateTimeInput.disabled = true;
+    getDeltaTime();
+}
 
 
 const options = {
@@ -29,7 +52,6 @@ const options = {
     onClose(selectedDates) {
         if (selectedDates[0] < CURRENT_DATE) {
             Notiflix.Notify.failure('Please choose a date in the future');
-            // window.alert('Please choose a date in the future');
         } else {
             refs.btnStart.disabled = false;
             SELECTED_DATE = selectedDates[0];
@@ -38,6 +60,7 @@ const options = {
 };
 
 flatpickr(refs.dateTimeInput, options);
+
 
 refs.btnStart.addEventListener('click', startTimer);
 
@@ -55,14 +78,10 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-function startTimer() {
-    refs.btnStart.disabled = true;
-    refs.dateTimeInput.disabled = true;
-    getDeltaTime();
-}
+
 
 function getDeltaTime() {
-    let timerId = setInterval(() => {
+        timerId = setInterval(() => {
         delta = SELECTED_DATE - Date.now();
         const dateOffset = convertMs(delta);
         
@@ -73,6 +92,8 @@ function getDeltaTime() {
         }
         }, 1000);
 }
+
+
 
 function clockView(dateOffset) {
     refs.days.textContent = dateOffset.days;
